7362fbee917777328c289d891f982813a3534793
jonbrand <jonbranaraujo18@gmail.com>
2022-03-29 16:43:38 -0300

    feat: add routes and fakeapi

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import './assets/css/base/base.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Page404 from './pages/Page404';
+import Header from './components/Header';
+
 
 function App() {
   return (
   <>
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
         <Route path='/about'>
           <About />
+        </Route>
+        <Route>
+          <Page404 />
         </Route>
       </Switch>
     </Router>
